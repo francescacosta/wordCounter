1ac632b83fe055ec8427b2e2979ffedb422a89fb
Francesca <francesca@thrivewb.com.au>
2018-11-24 13:48:14 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// upload = function() {
+//   console.log('test');
+//   var file = document.getElementById('file');
+// };
+//
+// var file = document.getElementById('file');
+//
+// var reader = new FileReader(File);
+//
+// reader.onload = function(e) {
+//   var text = reader.result;
+//   console.log(text);
+// };
+//
+// reader.readAsText(file);
+
+// var fs = require('fs'),
+//   out = console.log.bind(console);
+//
+// function main(filename, callback) {
+//   fs.readFile(filename, 'utf8', function(err, data) {
+//     if (err) throw err;
+//     callback(data.split('\n').length, data.split(/\s/).length);
+//   });
+// }
+// main(process.argv[2], function(lines, words) {
+//   out('%s is %s lines long, and has %s words.', process.argv[2], lines, words);
+// });
